61ad1aac0072df519784ab1ac2fe560caffd5391
tonylai16 <tonylai16@yahoo.com.hk>
2015-11-26 11:53:25 +0800

    Update

--- Password Management System/src/view/MainPage.java
+++ Password Management System/src/view/MainPage.java
@@ -6,6 +6,9 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -20,21 +23,21 @@ import javax.swing.border.EmptyBorder;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.ListSelectionModel;
 
+import model.SubAccount;
+
 public class MainPage extends JPanel {
 
 	private JPanel contentPane;
 	private JTable table;
-	private JTextField usernameText;
-	private JTextField domainText;
-	private JTextField passphraseText;
 	private JPasswordField keyText;
 	private JLabel message;
 	private DefaultTableModel model = new DefaultTableModel() {
-	    @Override
-	    public boolean isCellEditable(int row, int column) {
-	       //all cells false
-	       return false;
-	    }
+		@Override
+		public boolean isCellEditable(int row,int column){
+			if(column == 2) 
+				return false;//the 4th column is not editable
+			return true;
+		}
 	};
 	
 
@@ -43,12 +46,12 @@ public class MainPage extends JPanel {
 		
 		Font bold24 = new Font("Times New Roman", Font.BOLD, 24);
 		//JPanel panel = new JPanel();
-		setBounds(100, 100, 450, 430);
+		setBounds(0, 0, 700, 500);
 		setBorder(new EmptyBorder(5, 5, 5, 5));
 		setLayout(null);
 		
 		JLabel lblRegistration = new JLabel("Welcome, "+PMS.operation.getUsername());
-		lblRegistration.setBounds(0, 0, 438, 28);
+		lblRegistration.setBounds(161, 2, 438, 28);
 		add(lblRegistration);
 		lblRegistration.setHorizontalAlignment(SwingConstants.CENTER);
 		lblRegistration.setFont(bold24);
@@ -58,43 +61,19 @@ public class MainPage extends JPanel {
 		add(keyLabel);
 		
 		keyText = new JPasswordField();
-		keyText.setBounds(104, 40, 300, 25);
+		keyText.setBounds(104, 40, 344, 25);
 		add(keyText);
 		
-		JLabel domainLabel = new JLabel("Domain");
-		domainLabel.setBounds(14, 70, 80, 25);
-		add(domainLabel);
-		
-		domainText = new JTextField();
-		domainText.setBounds(104, 70, 300, 25);
-		add(domainText);
-		
-		JLabel usernameLabel = new JLabel("Username");
-		usernameLabel.setBounds(14, 100, 80, 25);
-		add(usernameLabel);
-		
-		usernameText = new JTextField();
-		usernameText.setBounds(104, 100, 300, 25);
-		add(usernameText);
-		
-		JLabel passphraseLabel = new JLabel("Passphrase");
-		passphraseLabel.setBounds(14, 130, 80, 25);
-		add(passphraseLabel);
-		
-		passphraseText = new JTextField();
-		passphraseText.setBounds(104, 130, 300, 25);
-		add(passphraseText);
-		
 		JButton addButton = new JButton("Add");
-		addButton.setBounds(10, 167, 117, 29);
+		addButton.setBounds(475, 83, 117, 29);
 		add(addButton);
 		
 		JButton updateButton = new JButton("Update");
-		updateButton.setBounds(147, 167, 117, 29);
+		updateButton.setBounds(475, 122, 117, 29);
 		add(updateButton);
 		
 		JButton deleteButton = new JButton("Delete");
-		deleteButton.setBounds(287, 167, 117, 29);
+		deleteButton.setBounds(475, 161, 117, 29);
 		add(deleteButton);
 		
 		message = new JLabel("");
@@ -114,8 +93,16 @@ public class MainPage extends JPanel {
         table.setVisible(true);
 		
 		JScrollPane scrollPane = new JScrollPane(table);	
-		scrollPane.setBounds(14, 230, 424, 182);
+		scrollPane.setBounds(14, 79, 438, 227);
 		add(scrollPane);
+		
+		JButton generateButton = new JButton("Generate");
+		generateButton.setBounds(475, 38, 117, 29);
+		add(generateButton);
+		
+		JButton saveButton = new JButton("Save Changes");
+		saveButton.setBounds(475, 234, 117, 72);
+		add(saveButton);
 
         
         AddButtonListener addListener = new AddButtonListener();
@@ -127,20 +114,70 @@ public class MainPage extends JPanel {
         
         DelButtonListener delListener = new DelButtonListener();
         deleteButton.addActionListener(delListener);
+        
+        saveButtonListener saveListener = new saveButtonListener();
+        saveButton.addActionListener(saveListener);
+        
+        generateButtonListener generateListener = new generateButtonListener();
+        generateButton.addActionListener(generateListener);
    
         RowClickListener rowClickListener = new RowClickListener();
         table.addMouseListener(rowClickListener);
 	}
-	
+	private class saveButtonListener implements ActionListener{
+		public void actionPerformed(ActionEvent event) {
+			message.setText("");
+			model = (DefaultTableModel) table.getModel();
+			model.addRow(new Object[] {"","",""});
+			int row = 0;
+			List<SubAccount> subAccount = new ArrayList<SubAccount>();
+			while(row < table.getRowCount()){
+					SubAccount temp = new SubAccount();
+					temp.setDomain(model.getValueAt(row, 0).toString());
+					temp.setUsername(model.getValueAt(row, 1).toString());
+					subAccount.add(temp);
+				row++;
+			}
+			try {
+				PMS.operation.saveChanges(subAccount);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+		} 
+	}
+	private class generateButtonListener implements ActionListener {
+		public void actionPerformed(ActionEvent event) {
+			message.setText("");
+			model = (DefaultTableModel) table.getModel();
+			int row = 0;
+			List<SubAccount> subAccount = new ArrayList<SubAccount>();
+			while(row < table.getRowCount()){
+					SubAccount temp = new SubAccount();
+					temp.setDomain(model.getValueAt(row, 0).toString());
+					temp.setUsername(model.getValueAt(row, 1).toString());
+					subAccount.add(temp);
+				row++;
+			}
+			List<SubAccount> temp = new ArrayList<SubAccount>();
+			temp = PMS.operation.keyGen(subAccount,keyText.getPassword().toString(),8);
+			row = 0;
+			while(row < table.getRowCount()){
+				model.setValueAt(temp.get(row).getPassword(), row, 2);
+			row++;
+		}
+		} 
+	}
+	public void displayPw(){
+		
+	}
 	private class AddButtonListener implements ActionListener {
 		public void actionPerformed(ActionEvent event) {
 			message.setText("");
 			model = (DefaultTableModel) table.getModel();
-			if (!domainText.getText().isEmpty() && !usernameText.getText().isEmpty() && !passphraseText.getText().isEmpty()) {
-				model.addRow(new Object[] {domainText.getText(),usernameText.getText(),passphraseText.getText()});
-			} else {
-				message.setText("Please enter every field.");
-			}
+			model.addRow(new Object[] {"","",""});
+			
 		} 
 	}
 	
@@ -148,21 +185,7 @@ public class MainPage extends JPanel {
 		public void actionPerformed(ActionEvent event) {
 			message.setText("");
 			model = (DefaultTableModel) table.getModel();
-			if (table.getSelectedRow() == -1) {
-				if (table.getRowCount() == 0 ) {
-					message.setText("Table is empty.");
-				} else {
-					message.setText("Please select an row to update.");
-				}
-			} else {
-				if (!domainText.getText().isEmpty() && !usernameText.getText().isEmpty() && !passphraseText.getText().isEmpty()) {
-					model.setValueAt(domainText.getText(), table.getSelectedRow(), 0);
-					model.setValueAt(usernameText.getText(), table.getSelectedRow(), 1);
-					model.setValueAt(passphraseText.getText(), table.getSelectedRow(), 2);
-				} else {
-					message.setText("Please enter every field.");
-				}
-			}
+			
 		} 
 	}
 	
@@ -181,17 +204,7 @@ public class MainPage extends JPanel {
 		public void mouseReleased(MouseEvent e) {
 			message.setText("");
 			model = (DefaultTableModel) table.getModel();
-			if (table.getSelectedRow() == -1) {
-				if (table.getRowCount() == 0 ) {
-					message.setText("Table is empty.");
-				} else {
-					message.setText("Please select an row to update.");
-				}
-			} else {
-				domainText.setText(model.getValueAt(table.getSelectedRow(), 0).toString());
-				usernameText.setText(model.getValueAt(table.getSelectedRow(), 1).toString());
-				passphraseText.setText(model.getValueAt(table.getSelectedRow(), 2).toString());
-			}
+			
 		}
 
 		@Override
