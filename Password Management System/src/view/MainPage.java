5233ebe034d4fa5d569db22f99fe603cedba9214
tonylai16 <tonylai16@yahoo.com.hk>
2015-11-26 14:41:42 +0800

    latest

--- Password Management System/src/view/MainPage.java
+++ Password Management System/src/view/MainPage.java
@@ -10,6 +10,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.swing.DefaultCellEditor;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -18,6 +19,8 @@ import javax.swing.JPasswordField;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.JTextField;
+import javax.swing.SpinnerModel;
+import javax.swing.SpinnerNumberModel;
 import javax.swing.SwingConstants;
 import javax.swing.border.EmptyBorder;
 import javax.swing.table.DefaultTableModel;
@@ -25,12 +28,15 @@ import javax.swing.ListSelectionModel;
 
 import model.SubAccount;
 
+import javax.swing.JSpinner;
+
 public class MainPage extends JPanel {
 
 	private JPanel contentPane;
 	private JTable table;
 	private JPasswordField keyText;
 	private JLabel message;
+	private JSpinner spinner;
 	private DefaultTableModel model = new DefaultTableModel() {
 		@Override
 		public boolean isCellEditable(int row,int column){
@@ -51,38 +57,34 @@ public class MainPage extends JPanel {
 		setLayout(null);
 		
 		JLabel lblRegistration = new JLabel("Welcome, "+PMS.operation.getUsername());
-		lblRegistration.setBounds(161, 2, 438, 28);
+		lblRegistration.setBounds(87, 2, 438, 28);
 		add(lblRegistration);
 		lblRegistration.setHorizontalAlignment(SwingConstants.CENTER);
 		lblRegistration.setFont(bold24);
 		
 		JLabel keyLabel = new JLabel("Master Key");
-		keyLabel.setBounds(14, 40, 80, 25);
+		keyLabel.setBounds(14, 40, 64, 25);
 		add(keyLabel);
 		
 		keyText = new JPasswordField();
-		keyText.setBounds(104, 40, 344, 25);
+		keyText.setBounds(86, 40, 223, 25);
 		add(keyText);
 		
 		JButton addButton = new JButton("Add");
 		addButton.setBounds(475, 83, 117, 29);
 		add(addButton);
 		
-		JButton updateButton = new JButton("Update");
-		updateButton.setBounds(475, 122, 117, 29);
-		add(updateButton);
-		
 		JButton deleteButton = new JButton("Delete");
-		deleteButton.setBounds(475, 161, 117, 29);
+		deleteButton.setBounds(475, 122, 117, 29);
 		add(deleteButton);
 		
 		message = new JLabel("");
 		message.setForeground(Color.RED);
-		message.setBounds(14, 202, 424, 16);
+		message.setBounds(14, 63, 424, 16);
 		add(message);
 		
         String[] columnsHeader = new String[] {
-                "Domain","Username","Passphrase"
+                "Domain","Username","Password"
             };
  
         model.setColumnIdentifiers(columnsHeader);
@@ -103,14 +105,20 @@ public class MainPage extends JPanel {
 		JButton saveButton = new JButton("Save Changes");
 		saveButton.setBounds(475, 234, 117, 72);
 		add(saveButton);
+		
+		spinner = new JSpinner();
+		spinner.setBounds(417, 40, 35, 28);
+		add(spinner);
+		SpinnerModel model = new SpinnerNumberModel(8, 8, 20,1);
+		spinner.setModel(model);
+		
+		JLabel lengthlabel = new JLabel("Password length");
+		lengthlabel.setBounds(319, 40, 100, 25);
+		add(lengthlabel);
 
         
         AddButtonListener addListener = new AddButtonListener();
         addButton.addActionListener(addListener);
-        
-        UpdateButtonListener updateListener = new UpdateButtonListener();
-
-        updateButton.addActionListener(updateListener);
         
         DelButtonListener delListener = new DelButtonListener();
         deleteButton.addActionListener(delListener);
@@ -123,48 +131,94 @@ public class MainPage extends JPanel {
    
         RowClickListener rowClickListener = new RowClickListener();
         table.addMouseListener(rowClickListener);
+        
+        
+        DefaultCellEditor singleclick = new DefaultCellEditor(new JTextField());
+        singleclick.setClickCountToStart(1);
+
+        //set the editor as default on every column
+        for (int i = 0; i < table.getColumnCount(); i++) {
+            table.setDefaultEditor(table.getColumnClass(i), singleclick);
+        } 
+	}
+	public boolean checkFieldEmpty(){
+		if (table.isEditing())
+		    table.getCellEditor().stopCellEditing();
+		model = (DefaultTableModel) table.getModel();
+		int row = 0;
+		boolean empty = false;
+		if (table.getRowCount()==0)
+			PMS.alertBox("You have no domain list ", "Error");
+		else{
+			while(row < table.getRowCount()){
+				if (model.getValueAt(row, 0).toString()==""){
+					empty = true;
+				}
+					
+				if (model.getValueAt(row, 1).toString()==""){
+					empty = true;
+				}
+					
+			row++;
+			}
+		}
+		if (empty == true)
+			PMS.alertBox("Missing domain information or username", "Missing fields");
+		if (String.valueOf(keyText)==null){
+			empty = true;
+			PMS.alertBox("You don't have a master key", "Missing fields");
+		}
+			
+		return empty;
 	}
 	private class saveButtonListener implements ActionListener{
 		public void actionPerformed(ActionEvent event) {
-			message.setText("");
-			model = (DefaultTableModel) table.getModel();
-			int row = 0;
-			List<SubAccount> subAccount = new ArrayList<SubAccount>();
-			while(row < table.getRowCount()){
-					SubAccount temp = new SubAccount();
-					temp.setDomain(model.getValueAt(row, 0).toString());
-					temp.setUsername(model.getValueAt(row, 1).toString());
-					subAccount.add(temp);
-				row++;
-			}
-			try {
-				PMS.operation.saveChanges(subAccount);
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+			if (!checkFieldEmpty())
+			{
+				message.setText("");
+				model = (DefaultTableModel) table.getModel();
+				int row = 0;
+				List<SubAccount> subAccount = new ArrayList<SubAccount>();
+				while(row < table.getRowCount()){
+						SubAccount temp = new SubAccount();
+						temp.setDomain(model.getValueAt(row, 0).toString());
+						temp.setUsername(model.getValueAt(row, 1).toString());
+						subAccount.add(temp);
+					row++;
+				}
+				try {
+					PMS.operation.saveChanges(subAccount);
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
 			}
 			
 		} 
 	}
 	private class generateButtonListener implements ActionListener {
 		public void actionPerformed(ActionEvent event) {
-			message.setText("");
-			model = (DefaultTableModel) table.getModel();
-			int row = 0;
-			List<SubAccount> subAccount = new ArrayList<SubAccount>();
-			while(row < table.getRowCount()){
-					SubAccount temp = new SubAccount();
-					temp.setDomain(model.getValueAt(row, 0).toString());
-					temp.setUsername(model.getValueAt(row, 1).toString());
-					subAccount.add(temp);
-				row++;
-			}
-			List<SubAccount> temp = new ArrayList<SubAccount>();
-			temp = PMS.operation.keyGen(subAccount,String.valueOf(keyText.getPassword()),8);
-			row = 0;
-			while(row < table.getRowCount()){
-				model.setValueAt(temp.get(row).getPassword(), row, 2);
-				row++;
+			if (!checkFieldEmpty())
+			{
+				message.setText("");
+				model = (DefaultTableModel) table.getModel();
+				int row = 0;
+				List<SubAccount> subAccount = new ArrayList<SubAccount>();
+				int length = (int) spinner.getValue();
+				while(row < table.getRowCount()){
+						SubAccount temp = new SubAccount();
+						temp.setDomain(model.getValueAt(row, 0).toString());
+						temp.setUsername(model.getValueAt(row, 1).toString());
+						subAccount.add(temp);
+					row++;
+				}
+				List<SubAccount> temp = new ArrayList<SubAccount>();
+				temp = PMS.operation.keyGen(subAccount,String.valueOf(keyText.getPassword()),length);
+				row = 0;
+				while(row < table.getRowCount()){
+					model.setValueAt(temp.get(row).getPassword(), row, 2);
+					row++;
+				}
 			}
 		} 
 	}
@@ -173,14 +227,6 @@ public class MainPage extends JPanel {
 			message.setText("");
 			model = (DefaultTableModel) table.getModel();
 			model.addRow(new Object[] {"","",""});
-			
-		} 
-	}
-	
-	private class UpdateButtonListener implements ActionListener {
-		public void actionPerformed(ActionEvent event) {
-			message.setText("");
-			model = (DefaultTableModel) table.getModel();
 			
 		} 
 	}
@@ -215,6 +261,7 @@ public class MainPage extends JPanel {
 			
 		}
 	} 
+	
 
 	private class DelButtonListener implements ActionListener {
 		public void actionPerformed(ActionEvent event) {
